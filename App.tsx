e87e851a8636fdb79f98d2a67d95737b80668946
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-13 22:10:49 +0200

    rendering Trip component successfully in the App component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import Day from "./components/Day";
+import TripData from "./components/testData/trips.json";
+import { useState, useEffect } from "react";
+import type { TripType } from "./components/types/TripType";
+import Trip from "./components/Trip";
 
 export default function App() {
+  const [trips, setTrips] = useState<TripType[]>();
+
+  useEffect(() => {
+    setTrips(TripData);
+  }, []);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Day></Day>
+      {trips?.map((trip) => {
+        return (
+          <Trip
+            belongsToCountryId={trip.belongsToCountryId}
+            days={trip.days}
+            rankeable={trip.rankeable}
+            title={trip.title}
+            tripId={trip.tripId}
+            key={trip.tripId}
+          ></Trip>
+        );
+      })}
     </View>
   );
 }
--- components/Trip.tsx
+++ components/Trip.tsx
@@ -33,3 +33,4 @@ function Trip({
     </>
   );
 }
+export default Trip;
