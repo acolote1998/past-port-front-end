c31f8b1a858577ff3614628ddf5756ce8dd6dc75
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-13 22:01:17 +0200

    basic rendering of Trip component

---
+++ components/Trip.tsx
@@ -0,0 +1,35 @@
+import Day from "./Day";
+import type { TripType } from "./types/TripType";
+import { View, Text } from "react-native";
+function Trip({
+  belongsToCountryId,
+  days,
+  rankeable,
+  title,
+  tripId,
+}: TripType) {
+  return (
+    <>
+      <View>
+        <Text>Title: {title}</Text>
+        <Text>Belongs To Country Id: {belongsToCountryId}</Text>
+        <Text>Rankeable : {rankeable}</Text>
+        <Text>Trip Id: {tripId}</Text>
+      </View>
+      {days.map((day) => (
+        <Day
+          dayId={day.dayId}
+          belongsToTripId={day.belongsToTripId}
+          title={day.title}
+          date={day.date}
+          rankeable={day.rankeable}
+          photos={day.photos}
+          description={day.description}
+          foodScore={day.foodScore}
+          joyScore={day.joyScore}
+          key={day.dayId}
+        />
+      ))}
+    </>
+  );
+}
