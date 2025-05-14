d5630c40510ce0d2b0be6d8e3617f7905a474ae1
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-05-14 08:24:57 +0200

    using SafeAreaProvider to avoid rendering in non-usable screen parts

--- App.tsx
+++ App.tsx
@@ -1,35 +1,18 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import TripData from "./components/testData/trips.json";
-import { useState, useEffect } from "react";
-import type { TripType } from "./components/types/TripType";
-import Trip from "./components/Trip";
+import { Main } from "./components/Main";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
-  const [trips, setTrips] = useState<TripType[]>();
-
-  useEffect(() => {
-    setTrips(TripData);
-  }, []);
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      {trips?.map((trip) => {
-        return (
-          <Trip
-            belongsToCountryId={trip.belongsToCountryId}
-            days={trip.days}
-            rankeable={trip.rankeable}
-            title={trip.title}
-            tripId={trip.tripId}
-            key={trip.tripId}
-          ></Trip>
-        );
-      })}
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Main></Main>
+      </View>
+    </SafeAreaProvider>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ components/Main.tsx
@@ -0,0 +1,31 @@
+import { View } from "react-native";
+import TripData from "./testData/trips.json";
+import { useState, useEffect } from "react";
+import type { TripType } from "./types/TripType";
+import Trip from "./Trip";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export function Main() {
+  const [trips, setTrips] = useState<TripType[]>();
+  const insets = useSafeAreaInsets();
+
+  useEffect(() => {
+    setTrips(TripData);
+  }, []);
+  return (
+    <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      {trips?.map((trip) => {
+        return (
+          <Trip
+            belongsToCountryId={trip.belongsToCountryId}
+            days={trip.days}
+            rankeable={trip.rankeable}
+            title={trip.title}
+            tripId={trip.tripId}
+            key={trip.tripId}
+          ></Trip>
+        );
+      })}
+    </View>
+  );
+}
